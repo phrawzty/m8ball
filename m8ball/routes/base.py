925d9365d33a80f88db3ab312821aff72eb8e3d6
phrawzty <phrawzty+github@gmail.com>
2014-07-17 11:31:09 +0200

    add comments

--- m8ball/routes/base.py
+++ m8ball/routes/base.py
@@ -5,6 +5,10 @@ from flask.ext import restful
 
 
 class Base(restful.Resource):
+    """This is the base URL which shoul return *something*."""
+
     def get(self):
+        """Perhaps this could display version info, help text, etc."""
+
         response = {'response': 'This is an API. :)'}
         return response
